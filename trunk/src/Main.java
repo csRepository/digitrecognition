88f07c518da2e547300a128f10379a385048679f
gltomasz <gltomasz@6f8e6da0-64aa-11de-bb41-d1906ad598ae>
2009-07-23 22:32:33 +0000

    Basic ANN framework.

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	private static Layer InputLayer;				// warstwa wejsciowa
 	private static Neuron Neuron;
 	private static Layer HiddenLayer;
+        private static int nIn,nHidd,nOut;
 
 
 	/**
@@ -15,38 +16,54 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
+                nIn=20;
+                nHidd=15;
+                nOut=10;
 		NeuralNet NeuralNetwork = new NeuralNet(); 	// tworzenie sieci neuronwej
-		NeuralNetwork.addLayer(21);	   				// dodanie warstwy wejsciowej o 20 neuronach
-		NeuralNetwork.addLayer(16);  				// dodanie warstwy ukrytej o 10 neuronach
-		NeuralNetwork.addLayer(10);  				// dodanie warstwy wyjsciowej o 10 neuronach
+		NeuralNetwork.addLayer(nIn);	   		// dodanie warstwy wejsciowej o 20 neuronach
+		NeuralNetwork.addLayer(nHidd);  		// dodanie warstwy ukrytej o 10 neuronach
+		NeuralNetwork.addLayer(nOut);  			// dodanie warstwy wyjsciowej o 10 neuronach
 		
 		InputLayer = NeuralNetwork.getLayer(0);
 		HiddenLayer = NeuralNetwork.getLayer(1);
 		OutputLayer = NeuralNetwork.getLayer(2);
 
 		Random randomGenerator = new Random();
-		for (int i=0;i<20;i++) {	
+		for (int i=0;i<nIn;i++) {
 			Neuron = (Neuron) InputLayer.getNeuron(i);
-			Neuron.setValue(randomGenerator.nextInt(2));
+			Neuron.setValue(randomGenerator.nextInt(2));    //losowe warstosci dla neuronow wejsciowych
 			//System.out.println(Neuron.getValue());
 			InputLayer.getNeuron(InputLayer.neurons.size()-1).setValue(1); //bias
 		}
-		
-		for (int i=0;i<15;i++) {
-			for (int j=0;j<20;j++) {
-				NeuralNetwork.connect(0, j, 1, i);
-				Neuron = (Neuron) HiddenLayer.getNeuron(i);
-				System.out.println("waga["+j+","+i+"]="+Neuron.inlinks.get(j).getValue());
+
+
+                //obliczenia dla warstwy ukrytej
+		for (int i=0;i<nIn;i++) {   //20
+			for (int j=0;j<nHidd;j++) { //15
+				NeuralNetwork.connect(0, i, 1, j);      //polaczenie neuronow
+				Neuron = (Neuron) HiddenLayer.getNeuron(j);
+				//System.out.println("waga["+j+","+i+"]="+Neuron.inlinks.get(i).getValue());
 			}
 		}
 		
-		for (int i=0;i<15;i++) {
+		for (int i=0;i<nHidd;i++) {
 			Neuron = (Neuron) HiddenLayer.getNeuron(i);
 			//Neuron.setValue(randomGenerator.nextInt(2));
 			Neuron.computeOutput();
-			System.out.println("neuron"+i+"="+Neuron.getValue());
+			System.out.println("neuronHidd("+i+")="+Neuron.getValue());
 		}
-			
+
+                //oblcizenia dla warstwy wyjsciowej
+                for (int i=0;i<nHidd;i++) {
+                    for (int j=0;j<nOut;j++) {
+                        NeuralNetwork.connect(1, i, 2, j);
+                    }
+                }
+                for (int i=0;i<nOut;i++) {
+                    Neuron = (Neuron) OutputLayer.getNeuron(i);
+                    Neuron.computeOutput();
+                    System.out.println("neuronOut("+i+")="+Neuron.getValue());
+                }
 	}
 }
--- trunk/src/NeuralNet.java
+++ trunk/src/NeuralNet.java
@@ -13,17 +13,28 @@ public class NeuralNet {
 
 	/**
 	 * Metoda dodaj¹ca warstwê do struktury sieci
-	 * @param n integer liczba neuronów
+	 * @param n liczba neuronów
 	 */
 	public void addLayer(int n) {
 		 //new Layer(n);
 		 layers.add(new Layer(n));
 	}
-	
+	/**
+         * Zwraca referencjê do warstwy
+         * @param n numer warstwy
+         * @return Layer
+         */
 	public Layer getLayer(int n) {
 		return (Layer) layers.get(n);
 	}
-	
+
+        /**
+         * Tworzy po³¹czenie pomiêdzy neuronami
+         * @param sourceLayer   numer warstwy, z której wychodzi po³¹czenie
+         * @param sourceNeuron  numer neuronu, z którego wychodzi po³¹czenie
+         * @param destLayer     numer warstwy docelowej po³¹czenia
+         * @param destNeuron    numer neuronu docelowego
+         */
 	public void connect(int sourceLayer,int sourceNeuron,
 		      int destLayer,int destNeuron)
 	{
