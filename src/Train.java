24f131e31867d273729e41d7c77cecc600d8c447
gltomasz <gltomasz@gmail.com>
2011-04-27 16:42:45 +0000

    Separating trunk from GUI branch(2).

--- src/Train.java
+++ src/Train.java
@@ -1,24 +1,8 @@
-/**
- *  Copyright 2010 Główczyński Tomasz
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License. 
- */
-
 import algorithms.AlgorithmsFactory;
 import algorithms.Propagation;
 import java.util.ArrayList;
-import model.database.MNISTDatabase;
-import model.neuralnetwork.*;
+import database.MNISTDatabase;
+import neuralnetwork.*;
 import util.*;
 
 /**
@@ -154,7 +138,6 @@ public class Train {
 
      private  static void prepareData(String typeOfSet, int patternsCount) {
         MNISTDatabase dataMNIST = MNISTDatabase.getInstance();
-        dataMNIST.initTrainData();
         double[][][] images = null; double[][] labels;
         ArrayList<Integer> trainArray = new ArrayList();
         ArrayList<Integer> pattNrs = new ArrayList();
