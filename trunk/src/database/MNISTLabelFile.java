fab8153f303eb9b9dc96afa059bcd8598e8e4839
gltomasz@gmail.com <gltomasz@gmail.com@6f8e6da0-64aa-11de-bb41-d1906ad598ae>
2011-04-27 16:09:02 +0000

    Separating trunk from GUI branch.

--- trunk/src/database/MNISTLabelFile.java
+++ trunk/src/database/MNISTLabelFile.java
@@ -1,29 +1,13 @@
-/**
- *  Copyright 2010 Główczyński Tomasz
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License. 
- */
-
-package model.database;
+package database;
 
 import java.io.*;
 
-public class MNISTLabelsFile extends RandomAccessFile {
+public class MNISTLabelFile extends RandomAccessFile {
   private int count;
   private int curr;
   private String fileName;
 
-  public MNISTLabelsFile(String fileName, String mode) throws IOException, FileNotFoundException {
+  public MNISTLabelFile(String fileName, String mode) throws IOException, FileNotFoundException {
     super(fileName,mode);
     this.fileName = fileName;
     if (this.readInt()!=2049) {
